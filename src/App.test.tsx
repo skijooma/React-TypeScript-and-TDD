b023a8f5eb607b886d0ef4555513d08b6cd049c1
Steven Kijooma <skijooma@andrew.cmu.edu>
2022-03-17 11:33:25 +0200

    Attempting TDD workflow.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App, {label} from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hello react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("generates a label", () => {
+  const result = label("React");
+  expect(result).toEqual("Hello REACT")
 });
