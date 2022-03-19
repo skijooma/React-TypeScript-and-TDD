6cf371d0e90fcf3f6d6ce335ed23bca689584899
Steven Kijooma <skijooma@andrew.cmu.edu>
2022-03-19 11:41:43 +0200

    step9: Testing with reich events.

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import {fireEvent, render} from "@testing-library/react";
 import {Counter} from "./Counter";
+import userEvent from "@testing-library/user-event";
+import exp from "constants";
 
 test("Should render a label and a counter", () => {
 	const { getByTitle } = render(<Counter />);
@@ -25,5 +27,21 @@ test("Should start at zero", () => {
 test("Should start at another value", () => {
 	const { getByTitle } = render(<Counter start={10}/>);
 	const counter = getByTitle("Current Count");
+	expect(counter).toHaveTextContent("10");
+});
+
+test("Should increment the count by one", () => {
+	const { getByTitle } = render(<Counter/>);
+	const counter = getByTitle("Current Count");
+	expect(counter).toHaveTextContent("0")
+	fireEvent.click(counter);
+	expect(counter).toHaveTextContent("1");
+});
+
+test("Should increment the counter by ten", () => {
+	const { getByTitle } = render(<Counter />);
+	const counter = getByTitle("Current Count");
+	expect(counter).toHaveTextContent("0");
+	userEvent.click(counter, { shiftKey: true });
 	expect(counter).toHaveTextContent("10");
 });
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -9,16 +9,22 @@ const initialState = { count: 0 };
 export type CounterState = Readonly<typeof initialState>;
 
 export class Counter extends Component<CounterProps, CounterState> {
-
 	readonly state: CounterState = initialState;
 
 	componentDidMount() {
 		if (this.props.start) {
 			this.setState({
 				count: this.props.start,
-			})
+			});
 		}
 	}
+
+	incrementCounter = (event: React.MouseEvent<HTMLElement>) => {
+		const inc: number = event.shiftKey ? 10 : 1;
+		this.setState({
+			count: this.state.count + inc,
+		});
+	};
 
 	render() {
 		const { label = "Count" } = this.props;
@@ -26,8 +32,12 @@ export class Counter extends Component<CounterProps, CounterState> {
 		return (
 			<div>
 				<span title="Count Label">{label}</span>
-				<span id="counter" title="Current Count">
-					{ this.state.count }
+				<span
+					id="counter"
+					title="Current Count"
+					onClick={this.incrementCounter}
+				>
+					{this.state.count}
 				</span>
 			</div>
 		);
